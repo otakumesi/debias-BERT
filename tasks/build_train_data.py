24b5ef8208cc5a9e93b520d7be6fc0d28d51cc54
Takuro Niitsuma <bakednt@gmail.com>
2020-10-07 16:41:57 +0900

    build datasets

--- tasks/build_train_data.py
+++ tasks/build_train_data.py
@@ -1,7 +1,53 @@
-ATTR_NOUNS = 'data/attributes.txt'
+import json
+import inflect
+
+p = inflect.engine()
+ATTR_NOUN_FILE = 'data/occupations.txt'
+
+SUBJECTS = [('he', 'she')]
+
+TEMPLATE_SENTENCES = [
+    '{target} is {occupation}.',
+    '{target} works as {occupation}.',
+    '{target} is interested in {occupation}.'
+]
+
+def add_article(noun):
+    if noun in ['he', 'she', 'they']:
+        return noun
+
+    if not p.singular_noun(noun):
+        return f'the {noun}'
+    else:
+        return f'the {p.singular_noun(noun)}'
+
+def should_add_article(noun):
+    if noun in ['he', 'she', 'they']:
+        return False
+    return True
+
 
 def main():
-    pass
+    with open(ATTR_NOUN_FILE) as f:
+        attrs = f.readlines()
+    subjects = SUBJECTS
+
+    attrs = [add_article(attr.rstrip('\n').lower()) for attr in attrs]
+
+    for sbjs in subjects:
+        for attr in attrs:
+            for sent in TEMPLATE_SENTENCES:
+                if should_add_article(sbjs[0]):
+                    mask = 'the [MASK]'
+                else:
+                    mask = '[MASK]'
+                biased_sent = sent.format(target = mask, occupation = attr)
+                standard_sent = sent.format(target = mask, occupation = 'the [MASK]')
+                dumped = json.dumps({'biased_sentence': biased_sent,
+                           'standard_sent': standard_sent,
+                           'targets': sbjs},
+                          indent=4)
+                print(dumped)
 
 
 if __name__ == '__main__':
