e921e1f6ef79a43673a7af56b29c4a4f4d320f34
Takuro Niitsuma <bakednt@gmail.com>
2020-10-19 19:25:46 +0900

    update

--- tasks/build_wiki_sentences.py
+++ tasks/build_wiki_sentences.py
@@ -1,19 +1,17 @@
 from tqdm import tqdm
-import requests
-from gensim.corpora import WikiCorpus
-
-DUMP_URL = 'https://dumps.wikimedia.org/enwiki/{date}/enwiki-{date}-pages-articles.xml.bz2'
-TMP_WIKI_PATH = '/tmp/wikidumps'
-
-res = requests.get(DUMP_URL.format(date = '20201001'), stream=True)
-with open(TMP_WIKI_PATH, 'wb') as f:
-    for chunk in tqdm(res.iter_content(chunk_size=1024)):
-        f.write(chunk)
-
-corpus = WikiCorpus(TMP_WIKI_PATH)
+import re
+from datasets import load_dataset
 
 WIKI_SENTENCES = 'data/wiki.sentences.txt'
+
+dataset = load_dataset("wiki40b", 'en', split='train')
+
 with open(WIKI_SENTENCES, 'w') as f:
-    for sents in tqdm(corpus.get_texts()):
-        for sent in sents:
-            f.write(f'{sent}\n')
+    for doc in dataset['text']:
+        paragraphs = re.findall(r'_START_PARAGRAPH_\n(.+)\n?', doc)
+        paragraphs = sum([para.split('_NEWLINE_') for para in paragraphs], [])
+        paragraphs = [para.lower() for para in paragraphs]
+        for paragraph in paragraphs:
+            sents = paragraph.split('_NEWLINE_')
+            for sent in sents:
+                f.write(f'{sent}\n')
