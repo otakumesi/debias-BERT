e95dd6434ff1bd7cd1cd0bac9e57281dd9a51c5d
otakumesi <bakednt@gmail.com>
2020-11-27 13:08:21 +0900

    update path to model codes

--- experiments/run_gendered_sent.py
+++ experiments/run_gendered_sent.py
@@ -85,8 +85,10 @@ def run():
         trainer.train(model_path=model_args.model_name_or_path)
         trainer.save_model(train_args.logging_dir)
 
-    system_output_dir = Path('runs') / 'models' / model_args.model_name_or_path / \
-            f"epoch_{train_args.num_train_epochs}_lr_{train_args.learning_rate}_batch_{train_args.per_device_train_batch_size}"
+    system_output_dir = Path('runs')
+    system_output_dir += model_args.model_name_or_path if model_args.model_name_or_path.startswith('models/') else 'models' / model_args.model_name_or_path
+    system_output_dir += f"epoch_{train_args.num_train_epochs}_lr_{train_args.learning_rate}_batch_{train_args.per_device_train_batch_size}"
+
     system_output_dir.mkdir(parents=True, exist_ok=True)
 
     if train_args.do_eval:
