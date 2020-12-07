67b7dcbf45f23a18bc1273ee34d72999a13319a9
Takuro Niitsuma <bakednt@gmail.com>
2020-12-07 18:01:24 +0900

    update crows_metric and add new classifiers

---
+++ experiments/classifiers.py
@@ -0,0 +1,66 @@
+from torch import nn
+from transformers import (
+    BertModel,
+    BertPreTrainedModel,
+    SequenceClassifierOutput
+)
+
+class BertForSequenceClassificationbyWholeLayer(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size * config.num_hidden_layers, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_hidden_states=True,
+            return_dict=True,
+        )
+
+        hidden_states = torch.stack(outputs.hidden_states).permute(1, 0, 2, 3)
+        pooled_outputs = self.model.pooler(hidden_states)
+        pooled_outputs = self.dropout(pooled_outputs)
+
+        logits = self.classifier(pooled_outputs.view(-1))
+
+        loss = None
+        if labels is not None:
+            if self.num_labels == 1:
+                #  We are doing regression
+                loss_fct = MSELoss()
+                loss = loss_fct(logits.view(-1), labels.view(-1))
+            else:
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states
+        )
