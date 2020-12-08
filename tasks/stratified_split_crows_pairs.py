93e4c885363b0808cb1cd3634f150dac9caaf790
otakumesi <bakednt@gmail.com>
2020-12-09 00:18:13 +0900

    update stratfiled splits

--- tasks/stratified_split_crows_pairs.py
+++ tasks/stratified_split_crows_pairs.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import pandas as pd
-from sklearn.model_selection import StratifiedShuffleSplit
-
+from sklearn.model_selection import train_test_split
 
 DATASET_FOLDER = Path("data")
 CROWS_PAIRS_PATH = DATASET_FOLDER / "crows_pairs_anonymized.csv"
@@ -9,10 +8,10 @@ CROWS_PAIRS_PATH = DATASET_FOLDER / "crows_pairs_anonymized.csv"
 
 def main():
     df = pd.read_csv(CROWS_PAIRS_PATH)
-    sss = StratifiedShuffleSplit(n_splits=1, train_size=0.7, test_size=0.3, random_state=42)
+    sss = (n_splits=1, train_size=0.7, test_size=0.3, random_state=42)
 
     columns = ["sent_more", "sent_less", "stereo_antistereo", "bias_type", "annotations", "anon_writer", "anon_annotators"]
-    for indeces_train, indeces_test in sss.split(df, df["bias_type"]):
+    for indeces_train, indeces_test in train_test_split(df, train_size=0.7, test_size=0.3, random_state=42, shuffle=True, df["bias_type"]):
         df[columns].iloc[indeces_train].to_csv(DATASET_FOLDER / "crows_pairs_train.csv")
         df[columns].iloc[indeces_test].to_csv(DATASET_FOLDER / "crows_pairs_test.csv")
 
