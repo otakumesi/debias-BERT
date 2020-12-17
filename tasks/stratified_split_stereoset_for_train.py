555069e016a82df62eaedb2ea8ae180de911e96a
otakumesi <bakednt@gmail.com>
2020-12-17 09:32:07 +0900

    update tasks

--- tasks/stratified_split_stereoset_for_train.py
+++ tasks/stratified_split_stereoset_for_train.py
@@ -10,11 +10,11 @@ def main():
     columns = ["idx", "stereo_antistereo", "bias_type", "sent_more", "sent_less", "target"]
     df = pd.read_csv(STEREOSET_PAIRS_PATH, names=columns, skiprows=1)
 
-    df_train, df_valid_test = train_test_split(df, train_size=0.7, test_size=0.3, random_state=42, shuffle=True, stratify=df[["bias_type", "target"]])
+    df_train, df_valid_test = train_test_split(df, train_size=0.9, test_size=0.1, random_state=42, shuffle=True, stratify=df[["bias_type", "target"]])
     print(f"--- output train_set... {len(df_train)} data ---")
     df_train.to_csv(DATASET_FOLDER / "stereoset_train.train.csv")
 
-    df_valid, df_test = train_test_split(df_valid_test, train_size=0.75, test_size=0.25, stratify=df_valid_test["bias_type"])
+    df_valid, df_test = train_test_split(df_valid_test, train_size=0.5, test_size=0.5, stratify=df_valid_test["bias_type"])
     print(f"--- output valid_set... {len(df_valid)} data ---")
     df_valid.to_csv(DATASET_FOLDER / "stereoset_train.valid.csv")
 
