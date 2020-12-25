66418378d81e77dfccb8f2b756bd0999f7c190e3
otakumesi <bakednt@gmail.com>
2020-12-25 11:13:05 +0900

    update build sentences

--- tasks/build_train_sentences.py
+++ tasks/build_train_sentences.py
@@ -9,42 +9,95 @@ config = BertConfig.from_pretrained('bert-base-uncased')
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', config=config)
 model = BertLMHeadModel.from_pretrained('bert-base-uncased', config=config)
 
-GENDER_NOUNS = ["woman", "man", "girl", "boy"]
+PRONOUNS = ["she", "he"]
+
+# because of avoiding bug on inflect, these are plural.
+GENDER_NOUNS = ["women",
+                "men",
+                "girls",
+                "boys",
+                "gentlemen",
+                "gentlewomen",
+                "businessmen",
+                "businesswomen"]
+
 RACE_NOUNS = ["black", "white", "african", "asian", "hispanic", "indian"]
-RELIGION_NOUNS = ['muslim', 'buddhist', 'christian', 'jewish']
+RELIGION_NOUNS = ['muslim', 'buddhist', 'christian', 'jew', "atheist", "catholic", "protestant"]
 
 TEMPLATE_SENTENCES = [
-    "[PERSON_SBJ] [VERB] very [MASK].",
     "I [VERB] [PERSON_SBJ] [MASK].",
-    "[PERSON_SBJ] is [PERSON_NOUN]. [PERSON_SBJ] is a nice [MASK].",
+    "[PERSON_SBJ] is [PERSON_NOUN]. [PERSON_SBJ] is a [ADJ] [MASK].",
     "[PERSON_SBJ] is [PERSON_NOUN]. [PERSON_SBJ] is a bad [MASK].",
     "[PERSON_SBJ] is [PERSON_NOUN]. [PERSON_OBJ] profession is a [MASK]."
 ]
 
-VERBS = ["is", "look"]
+ADVS = ["very"]
+ADJS = ["nice", "bad"]
+VERBS = ["is", "look", "like", "do"]
 
-def build_simple_sentence(verb, person):
-    return "[PERSON_SBJ] [VERB] very [MASK].".replace("[PERSON_SBJ]", person).replace("[VERB]", verb)
 
-def get_mask_topk_tokens(sents, k=30):
-    inputs = tokenizer(sents)
-    mask_indices = (inputs["input_ids"] == tokenizer.mask_token_id).nonzero()
+def sub_adj(sent, adj):
+    return sent.replace("[ADJ]", adj)
+
+def sub_verb(sent, verb, plural=False):
+    if plural:
+        return sent.replace("[VERB]", p.plural_verb(verb))
+    return sent.replace("[VERB]", verb)
+
+def sub_noun_sbj(sent, sbj, plural=False):
+    if sbj.lower() in ["he", "she", "they"]:
+        return sent.replace("[PERSON_SBJ]", sbj.capitalize())
+    if plural:
+        return sent.replace("[PERSON_SBJ]", p.plural(sbj).capitalize())
+    return sent.replace("[PERSON_SBJ]", p.a(sbj).capitalize())
+
+def sub_noun_obj(sent, obj, plural=False):
+    if sbj.lower() in ["him", "her", "them"]:
+        return sent.replace("[PERSON_SBJ]", sbj.capitalize())
+    if plural:
+        return sent.replace("[PERSON_SBJ]", p.plural(sbj).capitalize())
+    return sent.replace("[PERSON_SBJ]", p.a(sbj).capitalize())
+
+def build_svac(s, v, a, plural):
+    template = "[PERSON_SBJ] [VERB] [ADJ] [MASK]."
+    sent = sub_noun_sbj(template, s, plural=plural)
+    sent = sub_verb(sent, v, plural=plural)
+    sent = sub_adj(sent, a)
+    return sent
+
+def sub_mask(sent, token):
+    return sent.replace("[MASK]", token)
+
+
+def get_mask_topk_tokens(sents, k=10):
+    inputs = tokenizer(sents, return_tensors="pt", padding=True)
+    mask_indices = (inputs["input_ids"] == tokenizer.mask_token_id).nonzero()[:, 1].view(-1, 1, 1).repeat_interleave(dim=2, repeats=tokenizer.vocab_size)
     outputs = model(**inputs)
     logits = F.softmax(outputs.logits, dim=-1)
-    import ipdb; ipdb.set_trace()
     token_ids = logits.gather(dim=1, index=mask_indices).topk(k=k, dim=-1).indices
-    tokens = tokenizer.convert_ids_to_tokens()
+    tokens = tokenizer.convert_ids_to_tokens(token_ids.flatten().unique())
+
+    singular_nouns = set(RACE_NOUNS + RELIGION_NOUNS)
+    plural_nouns = {p.plural(noun) for noun in singular_nouns} | set(GENDER_NOUNS)
+    singular_nouns |= {p.singular_noun(noun) for noun in GENDER_NOUNS }
+
+    tokens = set(tokens) - (singular_nouns | plural_nouns)
     return tokens
 
 def build_sentences():
+    results = []
     for verb in VERBS:
-        sents = [build_simple_sentence(verb, person) for noun in RACE_NOUNS]
-        mask_tokens = get_mask_topk_tokens(sents)
+        for adj in ADJS:
+            sents = [build_svac(noun, verb, adj, False) for noun in PRONOUNS]
+            sents.extend([build_svac(noun, verb, adj, True) for noun in RACE_NOUNS + RELIGION_NOUNS])
+            mask_tokens = get_mask_topk_tokens(sents)
 
+            for sent in sents:
+                results.extend([sub_mask(sent, token) for token in mask_tokens])
 
-    mask_tokens.extend()
+    import ipdb; ipdb.set_trace()
 
     return json.dumps({'data': sent_dicts})
 
 if __name__ == '__main__':
-    print(build_sentence_dicts(DATASET_PATH))
+    print(build_sentences())
