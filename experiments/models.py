90d3bd10f12cd20f58f5cc8f3c9677dd9ff72666
otakumesi <bakednt@gmail.com>
2021-02-20 00:23:24 +0900

    put model codes back on the paper.

--- experiments/models.py
+++ experiments/models.py
@@ -6,6 +6,7 @@ import torch.nn.functional as F
 
 class SentencePertubationNormalizer(nn.Module):
     """
+    This layer imprements the ...
     """
     def __init__(self, model: nn.Module, k: float = 0.01):
         super().__init__()
@@ -41,11 +42,23 @@ class SentencePertubationNormalizer(nn.Module):
             return_dict=True,
         )
 
-        more_hidden_state = more_outputs.hidden_states[-1]
+        more_logits = more_outputs.logits * more_attention_mask.unsqueeze(2)
+        less_logits = less_outputs.logits * less_attention_mask.unsqueeze(2)
 
-        less_hidden_state = less_outputs.hidden_states[-1]
+        vocab_size = self.model.config.vocab_size
 
-        loss = (more_hidden_state.softmax(dim=-1) * F.mse_loss(less_hidden_state, more_hidden_state.detach(), reduction='none')).sum(dim=-1).mean()
+        more_logits_indices = more_indices.unsqueeze(2).repeat_interleave(dim=2, repeats=vocab_size)
+        more_logits = more_logits.gather(dim=1, index=more_logits_indices)
+        more_logits = more_logits * more_mask.unsqueeze(2)
+
+        less_logits_indices = less_indices.unsqueeze(2).repeat_interleave(dim=2, repeats=vocab_size)
+        less_logits = less_logits.gather(dim=1, index=less_logits_indices)
+        less_logits = less_logits * less_mask.unsqueeze(2)
+
+        more_log_probs = torch.log_softmax(more_logits, dim=-1)
+        less_log_probs = torch.log_softmax(less_logits, dim=-1)
+
+        loss = F.kl_div(less_log_probs, more_log_probs.detach(), log_target=True, reduction="batchmean")
 
         return (loss,)
 
